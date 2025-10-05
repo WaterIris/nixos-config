d09e57a8a705f6828bbceaadf2172b22acce3c15
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-10-05 17:57:36 +0200

    add comments to packages and update nixvim

--- home-manager/modules/packages.nix
+++ home-manager/modules/packages.nix
@@ -1,9 +1,10 @@
 {pkgs, inputs, ...}:
 {
   home.packages = with pkgs; [
-    inputs.nixvim.packages.${system}.default
-    kitty
-    tmux
-    ripgrep
+    inputs.nixvim.packages.${system}.default # enable nixvim flake
+    kitty # terminal emulator
+    tmux # multiple terminals
+    ripgrep # better grep
+    fd # better find
   ];
 }
